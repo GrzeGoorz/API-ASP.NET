746f56c7df24a4f496ad7adcd02173c963a2bb62
GrzeGoorz <grzegorz96k@gmail.com>
2023-12-07 21:29:39 +0100

    v2.26

--- Program.cs
+++ Program.cs
@@ -16,14 +16,19 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-// Buduje obiekt aplikacji
+
+builder.Services.AddCors();
+
+// Tworzy instancję aplikacji ASP.NET Core na podstawie konfiguracji zdefiniowanej w obiekcie builder.
 var app = builder.Build();
 
-// Włącza przekierowanie na protokół HTTPS
-app.UseHttpsRedirection();
+// Konfiguracja obsługi zapytań CORS (Cross-Origin Resource Sharing). co pozwala na żądania z innych domen niż ta, z której serwuje się zasoby. 
+// W tym przypadku, pozwala na żądania z dowolnych nagłówków (AllowAnyHeader()), dowolnych metod (AllowAnyMethod()),
+// oraz z określonego pochodzenia (WithOrigins("http://localhost:4200")), co jest często używane w aplikacjach internetowych na front-endzie.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
-app.UseAuthorization();
-
+// Mapuje kontrolery do ścieżek URL.
 app.MapControllers();
 
+// Uruchamia aplikację. Ta linia kodu jest odpowiedzialna za obsługę żądań HTTP.
 app.Run();
