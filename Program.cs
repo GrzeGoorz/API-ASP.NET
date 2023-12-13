0fb404e942bedc8b6711c51f0a3a2bbaa20d8bf8
GrzeGoorz <grzegorz96k@gmail.com>
2023-12-14 00:25:14 +0100

    v5.54

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Dodaje kontrolery do kontenera serwisowego.
 builder.Services.AddControllers();
+// Dodaje usługi aplikacji, takie jak DbContext, obsługa CORS i implementacja TokenService.
 builder.Services.AddApplicationServices(builder.Configuration);
+// Dodaje usługi związane z uwierzytelnianiem, takie jak konfiguracja uwierzytelniania JWT.
 builder.Services.AddIdentityServices(builder.Configuration);
 
 
@@ -19,7 +20,7 @@ var app = builder.Build();
 // Konfiguracja obsługi zapytań CORS (Cross-Origin Resource Sharing). co pozwala na żądania z innych domen niż ta, z której serwuje się zasoby. 
 // W tym przypadku, pozwala na żądania z dowolnych nagłówków (AllowAnyHeader()), dowolnych metod (AllowAnyMethod()),
 // oraz z określonego pochodzenia (WithOrigins("http://localhost:4200")), co jest często używane w aplikacjach internetowych na front-endzie.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
